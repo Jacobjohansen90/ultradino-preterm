14f56258070174bee7c35d48c658c8f0e05cbe9d
Jacob <jacobjohansen90@gmail.com>
2026-03-02 11:42:08 +0100

    Preprocessing done?

--- preprocessing/link_csv_and_db.py
+++ preprocessing/link_csv_and_db.py
@@ -9,12 +9,19 @@ Created on Tue Feb 24 12:40:03 2026
 import sqlite3
 import csv
 from datetime import datetime
-import matplotlib.pyplot as plt
+import numpy as np
+import json 
+import logging
+
+logging.basicConfig(filename="/projects/users/data/UCPH/DeepFetal/projects/preterm/log.log", filemode='w')
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 variables_from_csv = ['GestationsalderUger',
                       'Alder_Mor']
 
-variables_from_db = ['pixel_spacing_1']
+variables_from_db = [6,7,-1]
 
 working_dir = '/projects/users/data/UCPH/DeepFetal/projects/preterm/'
 
@@ -33,9 +40,8 @@ text_date = 'FoedselsDato_Barn'
 i_studydate = 5
 
 variables_i = []
-days = []
 not_found = []
-
+imgs = []
 for i in range(len(headers)):
     if headers[i] == text_cpr:
         i_cpr = i
@@ -43,24 +49,53 @@ for i in range(len(headers)):
         i_date = i
     elif headers[i] in variables_from_csv:
         variables_i.append(i)
+
+if len(variables_from_csv) != len(variables_i):
+    found = ([headers[i] for i in variables_i])
+    diff = list(set(found) - set(variables_from_csv))
+    raise Exception(f"Did not variables {diff} in CSV")
         
+i = 0
 for row in f_csv:
+    i += 1
+    if i % 1000 == 0:
+        logger.info(f"Completed {i} files")
+        break
+    
     cpr_phair = row[i_cpr]
     birthdate = datetime.strptime(str(row[i_date]).replace("-",""), "%Y%m%d").date()
     
     query = f"SELECT xxhash FROM cpr_hashes WHERE phair_hash = '{cpr_phair}'"
-    cpr_hash = list(cur.execute(query))[0][0]
-    query = f"SELECT * FROM metadata_cache WHERE file_hash = '{cpr_hash}"
-    entries = list(cur.execute(query))
+    cpr_hash = list(cur.execute(query))
     
-    for entry in entries:
-        study_date = datetime.strptime(str(entry[i_studydate]), "%Y%m%d").date()
-        days.append((study_date - birthdate).days)
+    if len(cpr_hash) == 0:
+        not_found.append(cpr_phair)
+    else:    
+        temp = {'cpr_phair': cpr_phair}
+        for i in range(len(variables_i)):
+            temp[variables_from_csv[i]] = row[variables_i[i]]
+        cpr_hash = cpr_hash[0][0]
+        query = f"SELECT * FROM metadata_cache WHERE file_hash = '{cpr_hash}'"
+        entries = list(cur.execute(query))
+        img_paths = []
+        for entry in entries:
+            study_date = str(entry[i_studydate])
+            study_date = datetime.strptime(study_date, "%Y%m%d").date()
+            if np.abs((study_date - birthdate).days) < 280:
+                ps1 = entry[6]
+                ps2 = entry[7]
+                img_path = entry[-1]
+                img_paths.append([img_path, ps1, ps2])
+        imgs[cpr_hash] = temp
+        
+with open(working_dir + 'preprocessing/missing.csv', 'w', newline='') as file:
+    wr = csv.writer(file, quoting=csv.QUOTE_ALL)
+    for row in not_found:
+        wr.writerow(row)
 
-plt.hist(days, density=True)
-plt.savefig(working_dir + 'preprocessing/histogram.png')
-        
-        
+
+with open(working_dir + 'preprocessing/data.json', 'w') as file:
+    json.dump(imgs, file)
         
         
         
