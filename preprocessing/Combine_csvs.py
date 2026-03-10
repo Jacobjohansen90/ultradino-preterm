ce02f6dfa631284500a483fe07ed80f755a5a2d4
Jacob Johansen <jacobjohansen90@gmail.com>
2026-03-10 19:24:41 +0100

    Csv combiner

--- preprocessing/Combine_csvs.py
+++ preprocessing/Combine_csvs.py
@@ -10,17 +10,36 @@ import csv
 
 working_path = "/projects/users/data/UCPH/DeepFetal/projects/preterm/Registers/"
 
-headers = ["cpr_child", "cpr_mother", "GA_weeks", "GA_days", "Age_mother", "Birthdate"]
+headers = ["cpr_child", "cpr_mother", "GA_days", "Age_mother", "Birthdate"]
 
-csvs = [["mfr.csv", ["CPR_BARN", "CPR_MODER", None, "GESTATIONSALDER_DAGE", "ALDER_MODER", "FOEDSELSDATO"]],
-        ["nyfoedte.csv", ["CPRnummer_Barn", "CPRnummer_Mor", "GestationsalderUger", "Gestationsalder", "Alder_Mor", "FoedselsDato_Barn"]]]
+csvs = [["mfr.csv", ["CPR_BARN", "CPR_MODER", "GESTATIONSALDER_DAGE", "ALDER_MODER", "FOEDSELSDATO"]],
+        ["nyfoedte.csv", ["CPRnummer_Barn", "CPRnummer_Mor", "Gestationsalder", "Alder_Mor", "FoedselsDato_Barn"]]]
 
-with open(working_path + csv[0]) as file:
+with open(working_path + 'data.csv') as file:
+    wr = csv.writer(file, quoting=csv.QUOTE_ALL)
+    wr.writerow(headers)
+
     for csv_info in csvs:
-        read_file = open(working_path + csv_info[0])
-        read_csv = csv.reader(read_file)
-        headers = next(read_csv)
+        idxs = []
+        csv_file = open(working_path + csv_info[0])
+        csv_headers = csv_info[1]
+        csv = csv.reader(csv_file)
+        temp_headers = next(csv)
         
-        for line in read_csv:
-            
-        
+        for head in csv_headers:
+            i = 0
+            for temp_head in temp_headers:
+                if temp_head == head:
+                    idxs.append(i)
+                    break
+                else:
+                    i += 1
+                
+        for row in csv:
+            info = []
+            for idx in idxs:
+                info.append(row[i])
+            wr.writerow(info)
+    
+    csv_file.close()
+    
