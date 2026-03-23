da79bc1e31835cd9cee60612a0b4a98d3ebb5644
Jacob Johansen <jacobjohansen90@gmail.com>
2026-03-23 22:24:06 +0100

    wtf is going on....

--- ultradino_preterm_study/NGC_validator.py
+++ ultradino_preterm_study/NGC_validator.py
@@ -19,9 +19,9 @@ import csv
 
 cfg = OmegaConf.load("Vit_Small_Img_Resampled_B2M_cervical.yaml")
 
-folds_path = "../all_folds/All_Folds_Spacing_CL2026-03-13_13-21-52/"
+folds_path = "../../all_folds37w/All_Folds_Spacing_CL2026-03-13_13-21-52/"
 
-data_path = '../data/cervix_data_all.json'
+data_path = '../../data/cervix_data_all.json'
 
 metrics = get_metrics('cuda')
 
@@ -37,7 +37,7 @@ ValLoader = DataLoader(ValData,
 
 
 
-f = open("data.csv")
+f = open("data.csv", 'w')
 wr = csv.writer(f)
 wr.writerow(['cpr', 'pred', 'label', 'fold'])
 
