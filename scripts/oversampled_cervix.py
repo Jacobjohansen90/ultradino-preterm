e70122f045234e92f804f39086a9bf3955342df7
Jacob Johansen <jacobjohansen90@gmail.com>
2026-04-21 11:53:44 +0200

    oversample imgs

---
+++ scripts/oversampled_cervix.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 21 11:15:57 2026
+
+@author: jacob
+"""
+
+import polars as pl
+
+seed=12
+
+path_to_train_data = '/projects/users/data/UCPH/DeepFetal/projects/preterm/Data/train.csv'
+path_to_img_data = '/projects/users/data/UCPH/DeepFetal/projects/preterm/Data/data_dump/img_data.csv'
+save_path = '/projects/users/data/UCPH/DeepFetal/projects/preterm/misc/5M_cervix_oversampled.csv'
+
+prefix = '/projects/users/data/UCPH/DeepFetal/ultrasound/PNG_pretrain/'
+
+img_data = pl.read_csv(path_to_img_data, ignore_errors=True)
+img_data = img_data[['file_path']].unique()
+
+cv_data = pl.read_csv(path_to_train_data)
+cv_data = cv_data[['file_path']].unique()
+
+cv_imgs = cv_data
+other_imgs = img_data.filter(pl.col('file_path').is_in(cv_data['file_path']))
+
+cv_sampled = cv_imgs.sample(n=2500000, with_replacement=True, seed=seed)
+other_imgs = other_imgs.sample(n=2500000, seed=seed)
+
+cv_sampled = cv_sampled.with_columns((pl.lit(prefix) + pl.col('file_path')).alias('file_path'))
+other_imgs = other_imgs.with_columns((pl.lit(prefix) + pl.col('file_path')).alias('file_path'))
+
+final_data = cv_sampled.vstack(other_imgs)
+
+final_data.write_csv(save_path, include_header=False)
