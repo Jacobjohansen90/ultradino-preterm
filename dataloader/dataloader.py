48a5e924905cc303b36c65c641946a7be7f5195e
Jacob Johansen <jacobjohansen90@gmail.com>
2026-04-21 14:43:05 +0200

    hygge med bugs

--- dataloader/dataloader.py
+++ dataloader/dataloader.py
@@ -128,9 +128,9 @@ class PreTermDataset(Dataset):
         except:
             print(self.prefix + data['file_path'])
             img = torch.Tensor(np.zeros((1,224,224)))
-            label = torch.Tensor([0.0])
+            label = torch.Tensor([0])
         img_data = torch.Tensor([data['physical_delta_x'], data['physical_delta_y']])
-
+        label.to(torch.int)
         
         return {'img': img, 'img_data': img_data, 'ehr_data': ehr_data, 'label': label}
     
