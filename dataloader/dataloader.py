b5cf0a7069d8d39f6bfa8a14c703dc71deb905f8
Jacob Johansen <jacobjohansen90@gmail.com>
2026-05-07 11:03:19 +0200

    Overfitting negation

--- dataloader/dataloader.py
+++ dataloader/dataloader.py
@@ -132,7 +132,7 @@ class PreTermDataset(Dataset):
         except:
             img = torch.Tensor(np.zeros((1,224,224)))
             label = torch.Tensor([0])
-            print(data['file_path'])
+            print(data['file_path'].item())
 
         #Prepare image metadata
         try:
