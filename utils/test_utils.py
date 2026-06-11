27e1204014c669c13ea7c7c253539703a3714f8c
Jacob Johansen <jacobjohansen90@gmail.com>
2026-06-11 11:17:58 +0200

    almost

--- utils/test_utils.py
+++ utils/test_utils.py
@@ -43,7 +43,8 @@ def test_model(folder_path, testdata_path, move=True):
     wr_all.writerow(['Sens@Spec_avg', 'Sens@Spec_max', 
                      'Sens_avg', 'Spec_avg',
                      'Sens_max', 'Spec_max'
-                     'Avg_cutoff', 'Max_cutoff', 
+                     'avg_cutoff', 'max_cutoff', 
+                     'avg_cutoff_val', 'max_cutoff_val',
                      'weights'])
     
     df_avg = pl.read_csv(folder_path + 'Avg_metrics.csv')
@@ -56,8 +57,8 @@ def test_model(folder_path, testdata_path, move=True):
         model.load_state_dict(torch.load(folder_path + 'weights/' + weights, weights_only=True))
         model.eval()
         
-        t_avg = df_avg[i+1]['SensAtSpec_cutoff'].item()
-        t_max = df_max[i+1]['SensAtSpec_cutoff'].item()
+        t_avg = df_avg[i]['SensAtSpec_cutoff'].item()
+        t_max = df_max[i]['SensAtSpec_cutoff'].item()
 
         metrics_avg = get_test_metrics(cfg, t_avg)
         metrics_max = get_test_metrics(cfg, t_max)
@@ -88,7 +89,8 @@ def test_model(folder_path, testdata_path, move=True):
             Sens_max = round(metrics_avg['Sens'].compute().item(), 3)
             Spec_max = round(metrics_avg['Spec'].compute().item(), 3)
             
-            wr_all.writerow([SensSpec_avg, SensSpec_max, Sens_avg, Spec_avg, Sens_max, Spec_max, cutoff_avg, cutoff_max, weights])
+            wr_all.writerow([SensSpec_avg, SensSpec_max, Sens_avg, Spec_avg, Sens_max, Spec_max, 
+                             cutoff_avg, cutoff_max, t_avg, t_max, weights])
 
     f_all.close()
     
@@ -101,13 +103,13 @@ def test_model(folder_path, testdata_path, move=True):
     
     with open(folder_path + 'test_top_5.csv', 'w') as f:
         wr = csv.writer(f)
-        wr.writerow(['Sens@Spec', 'type', 'Sens', 'Spec', 'weights'])
+        wr.writerow(['Sens@Spec', 'type', 'Sens', 'Spec', 'cutoff', 'cutoff_val', 'weights'])
 
         for i in range(5):
             idx, col, _ = top_5.row(i)
             row = df[idx]
             t = col.split('_')[1]
-            wr.writerow([row[col], t, row['Sens_'+t], row['Spec_'+t], row['weights']])
+            wr.writerow([row[col], t, row['Sens_'+t], row['Spec_'+t], row[t+'_cutoff'], row[t+'_cutoff_val'], row['weights']])
        
     if move:
         dst = folder_path.replace('Current', 'Tested')
