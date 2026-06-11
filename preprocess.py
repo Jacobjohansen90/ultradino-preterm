e91d4d0925f39e081544c500e69527f0f396032e
Jacob Johansen <jacobjohansen90@gmail.com>
2026-06-11 11:58:23 +0200

    almost

--- preprocess.py
+++ preprocess.py
@@ -56,7 +56,7 @@ logger.info(f"Found {df_pop['CPR_MOTHER'].n_unique()} mothers - " + str(datetime
 df_img = sqlite_extractor(cfg, list(df_pop['CPR_MOTHER'].unique()))
 
 #Link cervix preds and image df
-df_cervix_preds = pl.read_csv(cfg.paths.misc_dir + 'cervix_preds.csv', infer_schema=False)
+df_cervix_preds = pl.read_csv(cfg.paths.cervix_preds, infer_schema=False)
 df_img = df_img.join(df_cervix_preds, on='file_path', how='left')
 
 df_img.write_csv(cfg.paths.data_dir + 'data_dump/img_data.csv')
