4bec12ed8f33f93a2eefb6f27dba0a21c43cf588
Jacob Johansen <jacobjohansen90@gmail.com>
2026-06-12 12:03:14 +0200

    what is happening...

--- utils/preprocessing_functions.py
+++ utils/preprocessing_functions.py
@@ -142,7 +142,7 @@ def sqlite_extractor(cfg, cpr_mothers):
     #TODO: Currently we drop any flow image. Update this so they are instead marked
     for row in cur.fetchall():
         if any(s is not None and "[" in s for s in row):
-            continue:
+            continue
         else:
             rows.append(row)
 
