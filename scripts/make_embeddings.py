bca8688509da8bbae6437edbec285117363f9869
Jacob Johansen <jacobjohansen90@gmail.com>
2026-06-25 11:20:46 +0200

    test script

--- scripts/make_embeddings.py
+++ scripts/make_embeddings.py
@@ -52,19 +52,19 @@ def make_embeddings(path, save_path):
         
         dfs = []
         embeddings = {}
-        
-        for data in tqdm(Data):
-                
-            outputs = model(data['imgs'].to(cfg.device.type),
-                            data['img_data'].to(cfg.device.type),
-                            data['ehr_data'].to(cfg.device.type))
-        
-            dfs.append(pl.DataFrame({"img": data["IDs"],
-                                     "preterm_pred": outputs["preterm"].flatten().cpu().numpy(),
-                                     "preterm_label": data["labels"]["preterm"].flatten().cpu().numpy()}))
+        with torch.no_grad():
+            for data in tqdm(Data):
+                    
+                outputs = model(data['imgs'].to(cfg.device.type),
+                                data['img_data'].to(cfg.device.type),
+                                data['ehr_data'].to(cfg.device.type))
             
-            embeddings.update({id_: emb for id_, emb in zip(data["IDs"], outputs['vision_features'].to('cpu').tolist())})
-    
+                dfs.append(pl.DataFrame({"img": data["IDs"],
+                                         "preterm_pred": outputs["preterm"].flatten().cpu().numpy(),
+                                         "preterm_label": data["labels"]["preterm"].flatten().cpu().numpy()}))
+                
+                embeddings.update({id_: emb for id_, emb in zip(data["IDs"], outputs['vision_features'].to('cpu').tolist())})
+        
         pred_df = pl.concat(dfs)
         
         df_final = df.join(pred_df, left_on='no_ocr_preprocessed_file_path', right_on='imgs', how='left')
