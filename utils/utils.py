21f03928b437ab5b6e1743e4fe6794d637fb7ab1
Jacob Johansen <jacobjohansen90@gmail.com>
2026-07-10 14:19:20 +0200

    major loss rework

--- utils/utils.py
+++ utils/utils.py
@@ -13,7 +13,7 @@ def setup(cfg):
     if cfg.info.name is None:
         raise Exception("Model experiment must be named")
     
-    path = f"/projects/users/data/UCPH/DeepFetal/projects/preterm/training_runs/Running/{cfg.info.name}_{str(cfg.data.ga_cutoff_weeks)}/"
+    path = f"/projects/users/data/UCPH/DeepFetal/projects/preterm/training_runs/Running/{cfg.info.name}/"
     os.makedirs(path + 'weights/', exist_ok=False)
     if os.path.exists(path.replace('Running', 'Evaluated')):
         raise Exception("Model experiment exists in Evaluated folder.")
