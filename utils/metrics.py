b04f431ed1b3ed8a5ae2fb96dde92d2a771a2dab
Jacob Johansen <jacobjohansen90@gmail.com>
2026-07-13 23:01:52 +0200

    multi head

--- utils/metrics.py
+++ utils/metrics.py
@@ -29,6 +29,8 @@ class Metrics():
         self.save_path = save_path
         
     def update(self, outputs, data):
+        print(outputs)
+        print(data)
         for cutoff in self.cutoffs:
             self.dfs[str(cutoff)].append(pl.DataFrame({'cpr': data['IDs'],
                                                        'preds': outputs['preterm'][str(cutoff)]['preds'].cpu().numpy(),
@@ -52,27 +54,24 @@ class Metrics():
             fig.savefig(Path(self.save_path) / f"{agg}_metrics.png", dpi=300)
             plt.close(fig)
         
-    
     def log_metrics(self, train_loss, val_loss):
 
         row = {'train_loss': round(train_loss, 5), 'val_loss': round(val_loss, 5)}
 
         for cutoff in self.cutoffs:
             df = pl.concat(self.dfs[str(cutoff)])
+            print(df)
             patient_df = (df.group_by("cpr").agg([pl.col('preds').mean().alias('avg'),
                                                   pl.col('preds').max().alias('max'),
                                                   pl.col('label').first().alias('label')]))
-            
+            print('PATIENT_DF')
+            print(patient_df)
             labels = torch.tensor(patient_df['label'].to_numpy(), dtype=torch.int32)
             
             for agg in ['avg', 'max']:
                 preds = torch.tensor(patient_df[agg].to_numpy(), dtype=torch.float32)
-                print(preds)
-                print(labels)
                 self.metric.reset()          
                 result = self.metric(preds, labels)
-                print(result)
-                print(type(result))
                 sens_spec, sens_spec_cutoff = self.metric(preds, labels)
                 
                 self.metrics[agg][str(cutoff)]['SensAtSpec'].append(sens_spec.item())
@@ -95,10 +94,6 @@ class Metrics():
         self.dfs = {str(c): [] for c in self.cutoffs}
 
         
-        
-
-
-
 def get_metrics(cfg, t=0.5):
     metrics = {'Recall': tm.Recall(task='binary', threshold=t).to(cfg.device.type),
                'Specificity': tm.Specificity(task='binary', threshold=t).to(cfg.device.type),
