482afddd21712e97cc0ceb993d8e1fda233f909c
Jacob Johansen <jacobjohansen90@gmail.com>
2026-07-16 10:12:22 +0200

    testing module added

--- scripts/retest_models.py
+++ scripts/retest_models.py
@@ -9,9 +9,12 @@ Created on Thu Jun 25 09:32:36 2026
 from utils.test_utils import test_model
 import os
 
-path = 'projects/users/data/UCPH/DeepFetal/projects/preterm/training_runs/Evaluated/'
+path = '/projects/users/data/UCPH/DeepFetal/projects/preterm/training_runs/Evaluated/'
 
 folders = os.listdir(path)
 
 for folder in folders:
+    print(f"Testing {folder}")
     test_model(path + folder, move=False)
+    print("Done")
+    print()
