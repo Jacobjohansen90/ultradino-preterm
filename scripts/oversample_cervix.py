56adeac7f85bf85bfad33e1ac2094a18fdc80faa
Jacob Johansen <jacobjohansen90@gmail.com>
2026-08-04 12:18:43 +0200

    oversample cervix for foundation model

--- scripts/oversample_cervix.py
+++ scripts/oversample_cervix.py
@@ -7,35 +7,18 @@ Created on Tue Apr 21 11:15:57 2026
 """
 
 import polars as pl
-import os
 
-seed=12
+holdout_csv = '/projects/users/data/UCPH/DeepFetal/projects/common/splits/split_V4/test_split_0.15_2026-06-08.csv'
+img_data = '/projects/users/data/UCPH/DeepFetal/projects/preterm/Data/AnyPreg_June_v3/data_dump/img_data.csv'
+save_path = '/projects/users/data/UCPH/DeepFetal/projects/preterm/misc/cervix.csv'
 
-path_to_train_data = '/projects/users/data/UCPH/DeepFetal/projects/preterm/Data/train.csv'
-path_to_img_data = '/projects/users/data/UCPH/DeepFetal/projects/preterm/Data/data_dump/img_data.csv'
-save_path = '/projects/users/data/UCPH/DeepFetal/projects/preterm/misc/5M_cervix_oversampled.csv'
+df_hold = pl.read_csv(holdout_csv)
+df = pl.read_csv(img_data)
 
-prefix = '/projects/users/data/UCPH/DeepFetal/ultrasound/PNG_pretrain/'
+df = df.filter(pl.col("pred") == 14)
 
-img_data = pl.read_csv(path_to_img_data, ignore_errors=True)
-img_data = img_data[['file_path']].unique()
+df = df.join(df_hold, left_on='CPR_MOTHER', right_on='CPR_MOR', how='anti')
 
-cv_data = pl.read_csv(path_to_train_data)
-cv_data = cv_data[['file_path']].unique()
+df = df['no_ocr_preprocessed_file_path']
 
-cv_imgs = cv_data
-other_imgs = img_data.filter(pl.col('file_path').is_in(cv_data['file_path']).not_())
-
-cv_sampled = cv_imgs.sample(n=2500000, with_replacement=True, seed=seed)
-other_imgs = other_imgs.sample(n=2500000, seed=seed)
-
-cv_sampled = cv_sampled.with_columns((pl.lit(prefix) + pl.col('file_path')).alias('file_path'))
-other_imgs = other_imgs.with_columns((pl.lit(prefix) + pl.col('file_path')).alias('file_path'))
-
-final_data = cv_sampled.vstack(other_imgs)
-
-final_data = final_data.filter(pl.col('file_path').map_elements(os.path.isfile))
-
-print(f"Writing {final_data.height} filepaths to csv")
-
-final_data.write_csv(save_path, include_header=False)
+df.write_csv(save_path, include_header=False)
