e8c14f0f7a55a08abf9dab008e4622ed0fa2466b
Jacob Johansen <jacobjohansen90@gmail.com>
2026-08-05 11:26:50 +0200

    stats

---
+++ scripts/population_stats.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 12 13:43:18 2026
+
+@author: jacob
+"""
+
+import polars as pl
+
+path = '/projects/users/data/UCPH/DeepFetal/projects/preterm/'
+
+train_data = '/projects/users/data/UCPH/DeepFetal/projects/preterm/Data/AnyPreg_June_v3/train.parquet'
+test_data = '/projects/users/data/UCPH/DeepFetal/projects/preterm/Data/OnlyFirstPreg_June_v3/test.parquet'
+
+remove_on_GA = ['c-section', 'induced']
+cutoffs = [32,34,37]
+
+train_df = pl.read_parquet(train_data)
+test_df = pl.read_parquet(test_data)
+
+print('--Training--')
+
+for GA in cutoffs:
+    print(f"--{GA}--")
+    df_all = train_df.filter((pl.col("GA") // 7 >= GA) | ((pl.col("GA") // 7 < GA) & pl.all_horizontal(~pl.col(remove_on_GA))))
+    df_preterm = train_df.filter((pl.col("GA") // 7 < GA) & pl.all_horizontal(~pl.col(remove_on_GA)))
+    #Population count
+    print(f"Total births: {df_all['CPR_CHILD'].n_unique()}")
+    print(f"Preterm births: {df_preterm['CPR_CHILD'].n_unique()}")
+    
+    #Age
+    print(f"Total Age (+/- SD): {round(df_all['AGE'].mean(), 2)} ({round(df_all['AGE'].std(), 2)}")
+    print(f"Preterm Age (+/- SD): {round(df_preterm['AGE'].mean(), 2)} ({round(df_preterm['AGE'].std(), 2)}")
+
+    #BMI
+    print(f"Total BMI (+/- SD): {round(df_all['BMI'].mean(), 2)} ({round(df_all['BMI'].std(), 2)}")
+    print(f"Preterm BMI (+/- SD): {round(df_preterm['BMI'].mean(), 2)} ({round(df_preterm['BMI'].std(), 2)}")
+
+
+
+
+
+    
