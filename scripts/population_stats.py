0a5c45bb6c200e7a2da87a83df496b82217be2b8
Jacob Johansen <jacobjohansen90@gmail.com>
2026-08-05 13:33:56 +0200

    stats

--- scripts/population_stats.py
+++ scripts/population_stats.py
@@ -97,10 +97,6 @@ for GA in cutoffs:
     print()
     #Scanners
     print('Scanners')
-    t = len(df_all) // 100
-
-    counts_all = df_all["manufacturer_model_name"].value_counts()
-    included_models = set(counts_all.filter(pl.col("count") >= t)["manufacturer_model_name"].to_list())    
     print_scanner_counts(df_all, "Total:", included_models)
     print_scanner_counts(df_preterm, "Preterm:", included_models)
     
