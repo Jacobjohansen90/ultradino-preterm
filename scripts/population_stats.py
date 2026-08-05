ae58138f47fb91e7201aeabcd5648f40475e9e52
Jacob Johansen <jacobjohansen90@gmail.com>
2026-08-05 13:09:05 +0200

    stats

--- scripts/population_stats.py
+++ scripts/population_stats.py
@@ -42,14 +42,26 @@ for GA in cutoffs:
     print()
     #Scanners
     print('Scanners')
+    print('Total:')
     t = len(df_all) // 100
     counts = df_all['manufacturer_model_name'].value_counts()
     included = counts.filter(pl.col("count") >= t)
     excluded = counts.filter(pl.col("count") < t)
     for row in included.iter_rows(named=True):
-        print(f"{row['manufacturer_model_name']}: {row['count']}")
+        print(f"\t{row['manufacturer_model_name']}: {row['count']}")
         
-    print(f"other: {excluded['count'].sum()}")
+    print(f"\tother: {excluded['count'].sum()}")
+    
+    print('Preterm:')
+    t = len(df_preterm) // 100
+    counts = df_preterm['manufacturer_model_name'].value_counts()
+    included = counts.filter(pl.col("count") >= t)
+    excluded = counts.filter(pl.col("count") < t)
+    for row in included.iter_rows(named=True):
+        print(f"\t{row['manufacturer_model_name']}: {row['count']}")
+        
+    print(f"\tother: {excluded['count'].sum()}")
+    
     print()
     
 print()    
@@ -60,16 +72,43 @@ for GA in cutoffs:
     df_all = test_df.filter((pl.col("GA") // 7 >= GA) | ((pl.col("GA") // 7 < GA) & pl.all_horizontal(~pl.col(remove_on_GA))))
     df_preterm = test_df.filter((pl.col("GA") // 7 < GA) & pl.all_horizontal(~pl.col(remove_on_GA)))
     #Population count
+    print('Births:')
     print(f"Total births: {df_all['CPR_CHILD'].n_unique()}")
     print(f"Preterm births: {df_preterm['CPR_CHILD'].n_unique()}")
-    
+    print()
     #Age
+    print('Age')
     print(f"Total Age (+/- SD): {round(df_all['AGE'].mean(), 2)} ({round(df_all['AGE'].std(), 2)})")
     print(f"Preterm Age (+/- SD): {round(df_preterm['AGE'].mean(), 2)} ({round(df_preterm['AGE'].std(), 2)})")
-
+    print()
     #BMI
+    print('BMI')
     print(f"Total BMI (+/- SD): {round(df_all['BMI'].mean(), 2)} ({round(df_all['BMI'].std(), 2)})")
     print(f"Preterm BMI (+/- SD): {round(df_preterm['BMI'].mean(), 2)} ({round(df_preterm['BMI'].std(), 2)})")
+    print()
+    #Scanners
+    print('Scanners')
+    print('Total:')
+    t = len(df_all) // 100
+    counts = df_all['manufacturer_model_name'].value_counts()
+    included = counts.filter(pl.col("count") >= t)
+    excluded = counts.filter(pl.col("count") < t)
+    for row in included.iter_rows(named=True):
+        print(f"\t{row['manufacturer_model_name']}: {row['count']}")
+        
+    print(f"\tother: {excluded['count'].sum()}")
+    
+    print('Preterm:')
+    t = len(df_preterm) // 100
+    counts = df_preterm['manufacturer_model_name'].value_counts()
+    included = counts.filter(pl.col("count") >= t)
+    excluded = counts.filter(pl.col("count") < t)
+    for row in included.iter_rows(named=True):
+        print(f"\t{row['manufacturer_model_name']}: {row['count']}")
+        
+    print(f"\tother: {excluded['count'].sum()}")
+    
+    print()
     
     
 
