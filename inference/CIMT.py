5fd13bfceb198f66d70122345ef989fd164afdde
Jacob Johansen <jacobjohansen90@gmail.com>
2026-08-26 10:54:02 +0200

    CIMT

--- inference/CIMT.py
+++ inference/CIMT.py
@@ -7,15 +7,20 @@ Created on Mon Aug 24 12:30:43 2026
 """
 
 from utils.model_utils import model_from_conf
+from omegaconf import OmegaConf
 import torch
 
-def inference(cfg, data, weight_path, cutoff):
+def load_model(cfg, weight_path):
     model = model_from_conf(cfg)
     model.load_state_dict(torch.load(weight_path, weights_only=True))
     model.eval()
-    outputs, _ = model(data['imgs'].to(cfg.device.type),
-                       data['img_data'].to(cfg.device.type),
-                       data['ehr_data'].to(cfg.device.type))
+    return model
+    
+
+def inference(model, data, cutoff, device_type='cude'):
+    outputs, _ = model(data['imgs'].to(device_type),
+                       data['img_data'].to(device_type),
+                       data['ehr_data'].to(device_type))
 
     pred = outputs['preterm'][str(cutoff)]['preds'].flatten().cpu().numpy()
     
