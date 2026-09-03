bacb4b193092f222e31648290a1f26deea3d83db
Jacob Johansen <jacobjohansen90@gmail.com>
2026-09-03 13:30:32 +0200

    x-fold val

--- utils/utils.py
+++ utils/utils.py
@@ -14,10 +14,9 @@ def setup(cfg):
         raise Exception("Model experiment must be named")
     
     path = f"/projects/users/data/UCPH/DeepFetal/projects/preterm/training_runs/Running/{cfg.info.name}/"
-    if cfg.info.name == 'test':
-        os.makedirs(path + 'weights/', exist_ok=True)
-    else:
-        os.makedirs(path + 'weights/', exist_ok=False)
+    if cfg.info.name != 'test':
+        if os.path.exists():
+            raise Exception("Model experiment exists in Running folder.")
         if os.path.exists(path.replace('Running', 'Evaluated')):
             raise Exception("Model experiment exists in Evaluated folder.")
     for i in range(cfg.data.folds):
