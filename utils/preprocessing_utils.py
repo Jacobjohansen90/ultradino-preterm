326025c5aa14035238fcecb2ed0bd5bbe1d91238
Jacob Johansen <jacobjohansen90@gmail.com>
2026-09-09 11:07:04 +0200

    preprocess

--- utils/preprocessing_utils.py
+++ utils/preprocessing_utils.py
@@ -103,6 +103,7 @@ def filter_conditions(df, condition, filter_on, table, action, external=True):
 def filter_df(df, criteria):
     table = None
     for condition in criteria.conditions:
+        print(condition)
         if 'table' in condition:
             df_temp = load_table(condition.table)
             table = filter_conditions(df_temp, condition, criteria.filter_on, table, criteria.action)
