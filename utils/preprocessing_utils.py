5b995ffbfc95da3c65eb0747cc053eecccc2fb5b
Jacob Johansen <jacobjohansen90@gmail.com>
2026-09-10 14:07:26 +0200

    preprocess

--- utils/preprocessing_utils.py
+++ utils/preprocessing_utils.py
@@ -110,13 +110,14 @@ def filter_df(df, criteria):
     print(criteria.name)
 
     df = df.with_columns(pl.lit(None, dtype=pl.Boolean).alias("remove"))
-    children_before = (
-    df
-    .filter(pl.col("remove") != True)
-    .select("CPR_CHILD")
-    .n_unique()
-)
+
     for action in criteria.actions: 
+        children_before = (
+        df
+        .filter(pl.col("remove") != True)
+        .select("CPR_CHILD")
+        .n_unique()
+    )
         table = None
         filter_on = [action.filter_on] if isinstance(action.filter_on, str) else action.filter_on
         for condition in action.conditions:
