929e8679a15f2ef5cbc37dec6e7043b1a55bd17b
Jacob Johansen <jacobjohansen90@gmail.com>
2026-09-22 10:25:25 +0200

    prog update

--- dataloader/dataloader.py
+++ dataloader/dataloader.py
@@ -296,8 +296,8 @@ class DataSplits:
 
         train_df = self.test_df.filter(pl.col('fold') != fold)
         train_df = pl.concat([self.train_df, train_df])
-        print(train_df.filter(pl.col("GA").is_null()))
-        print(train_df.filter(pl.col("GA").is_null()).height)
+        print(test_df.filter(pl.col("GA").is_null()))
+        print(test_df.filter(pl.col("GA").is_null()).height)
         for col in ["CPR_MOTHER", "CPR_CHILD", "file_path"]:
             overlap = (set(train_df[col].drop_nulls().unique()) & set(test_df[col].drop_nulls().unique()))
             
